80ecfe911ab4dd088af55bcac5c884da6036222c
Matsukawa Izumi <matsukawa@griffin-net.co.jp>
2020-06-16 13:39:29 +0900

    13:39

--- js/oyatsu.js
+++ js/oyatsu.js
@@ -4,7 +4,7 @@
   const choices = document.getElementById('choices');
   const btn = document.getElementById('btn');
   const fin = document.getElementById('fin');
-  const scoreLabel = document.querySelector('#result>p');
+  // const scoreLabel = document.querySelector('#result>p');
   // -----------------↓タイマー---------------------
   const timer = document.getElementById('timer');
   // -----------------↓スコア❤---------------------
@@ -29,27 +29,14 @@
           this.parentNode.removeChild(this);
         }, false);
       }
-      //-----------なでなでゲーム---------
+
+      //-----------おやつあてゲーム---------
 if(window.location.href.endsWith('play.html')){//あそび画面に居たら
-
-  //配列
-  const quiz= shuffle([
-    {q: 'img/L_dog.png', c: ['左手','右手',]},//0問目
-    {q: 'img/R_dog.png', c: ['左手','右手',]},
-
-  ]);
-  
-  const scoreHeartList = ['img/score0.png','img/score1.png', 'img/score2.png', 'img/score3.png',];
-  
-  //変数宣言
-  let currentNum = 0; //今何問目？
-  let isAnswered;
-  let score = 0;
   // -----------------↓タイマー---------------------
   let startTime;
   let timeoutId;
   let timeLimit = 6*1000 ;
-    
+  
     //タイマーのカウントダウン関数
     function countDown(){
       const d = new Date(startTime + timeLimit - Date.now());
@@ -57,53 +44,48 @@ if(window.location.href.endsWith('play.html')){//あそび画面に居たら
       
       timer.textContent = `${s}`;
       timeoutId = setTimeout(()=>{countDown();}, 10);
-  
+      
       //タイマーリセット条件・タイムオーバー処理
       if(isAnswered){//選択肢押したら
         clearTimeout(timeoutId);//タイマー停止。
         timer.textContent = "5";//タイマー表示を5に戻す。
-      }else if(timer.textContent === "59"){//5秒経ったら
+      }else if(timer.textContent === "0"){//5秒経ったら
         clearTimeout(timeoutId);//タイマー停止。
         isAnswered = true;//選択肢を押せなくなる。
         btn.classList.remove("disabled");//「つぎ」を押せるようにする。
         
       }     
     }
+
+    // window.onload = function(){//画面を読み込んだらタイマースタート 
+    //   startTime = Date.now();
+    //   countDown();
+    // }
+
+    // -----------------↑タイマー---------------------  
     
-    window.onload = function(){//画面を読み込んだらタイマースタート 
-      startTime = Date.now();
-      countDown();
-    }
+    const quiz=['img/L_dog.png','img/R_dog.png','img/L_dog.png','img/R_dog.png'];
+    const answer= ['左手','右手',];
+    const scoreHeartList = ['img/score0.png','img/score1.png', 'img/score2.png', 'img/score3.png',];
     
+    let currentNum = 0; //今何問目？
+    let isAnswered;
+    let score = 0;
+    let shuffled_quiz;//問題がでるたびに再代入していく。
     
-    //シャッフル関数
-    function shuffle(arr){//配列を入れるarr
-      
-      for(let i = arr.length - 1; i > 0; i--){//2.1.0
-        const j = Math.floor(Math.random()*(i+1));//2.1.0
-        [arr[i],arr[j]] = [arr[j],arr[i]];
+    //犬画像シャッフル関数
+    const shuffle = ([...quiz])=>{
+      for(let i = quiz.length - 1; i > 0; i--){//1.0
+        const j = Math.floor(Math.random()*(i+1));//1.0
+        [quiz[i],quiz[j]] = [quiz[j],quiz[i]];
       }
-      
-      return arr;
+      return quiz;
     }
-    
-    
-    function checkAnswer(li){//選択肢をクリックしたあとの〇✖表示
-      if(isAnswered){
-        return;//回答済みなら何も起きない。
-      }
-      isAnswered = true;//回答済みとする
-      
-      if(li.textContent === quiz[currentNum].c[0]){//クリックした選択肢がその問題のひとつめの選択肢と一致していたら
-        li.classList.add('correct');
-        score++;
-        scoreHeart.src = scoreHeartList[score];
         
-      }else{
-        li.classList.add('wrong');
-      }
-      
-      btn.classList.remove('disabled');//「つぎ」押せるようにする
+    function beforeQuiz(){
+      console.log('beforeQuizします。');
+      question.src = "img/0dog.pnd";
+      question.classList.add('transration');
     }
     
     function setQuiz(){//クイズ表示関数
@@ -112,51 +94,78 @@ if(window.location.href.endsWith('play.html')){//あそび画面に居たら
       startTime = Date.now();//タイマースタート
       countDown();
       
-      // question.textContent = quiz[currentNum].q;//問題文表示
-      question.src = quiz[currentNum].q;//問題画像表示
+      shuffled_quiz = (shuffle(quiz)[currentNum]);//再代入
+      
+      console.log(shuffled_quiz);
+      
+      question.src = shuffled_quiz;//問題画像表示
       
       while(choices.firstChild){//他問題の選択肢表示は全消去
         choices.removeChild(choices.firstChild);
       }
       
-      const shuffledChoices = shuffle([...quiz[currentNum].c]);//問題と選択肢の表示される並びをシャッフルしたもの。
-      
-      shuffledChoices.forEach(choice=>{//各選択肢に対して
+      answer.forEach(choice=>{//各選択肢に対して
         
         const li= document.createElement('li');//liをつくる
         
-      li.textContent = choice;//liの文章に選択肢の文章を代入する。
-  
-      li.addEventListener('click', ()=>{//選択肢がクリックされたら...
-        checkAnswer(li);
-        // li.classList.add("disabled_c");
+        li.textContent = choice;//liの文章に選択肢の文章を代入する。
+        
+        li.addEventListener('click', ()=>{//選択肢がクリックされたら...
+          checkAnswer(li);
+        });
+        
+        choices.appendChild(li);//ulの下にliを追加する。
+        
+        
       });
       
-      choices.appendChild(li);//ulの下にliを追加する。
-      
-      
-    });
-    
-    
-    
-    if(currentNum === quiz.length - 1){//最後の一問の時
-      // btn.textContent = 'Show Score';
-      btn.classList.add('disabled');//「つぎ」押せなくする。
+      if(currentNum === quiz.length - 2){//三問終了済
+        btn.classList.add('disabled');//「つぎ」押せなくする。
+      }
     }
-  }
-  
-  setQuiz();
-  
-  btn.addEventListener('click', ()=>{//「つぎ」ボタンを押したとき
+
+    // beforeQuiz();
+    setQuiz();
     
-    if(btn.classList.contains('disabled')){//未回答のときは無。
+    function checkAnswer(li){//選択肢をクリックしたら
+      
+      if(isAnswered){
+        return;//回答済みなら何も起きない。
+      }
+      isAnswered = true;//回答済みとする
+
+      
+      if(shuffled_quiz === 'img/L_dog.png'){
+        if(li.textContent === answer[0]){//左犬でc[0]選んだら
+          li.classList.add('correct');
+          score++;
+          scoreHeart.src = scoreHeartList[score];
+        }else{
+          li.classList.add('wrong');
+        }
+      }else if(shuffled_quiz === 'img/R_dog.png'){
+        if(li.textContent === answer[1]){//右犬でc[1]選んだら
+          li.classList.add('correct');
+          score++;
+          scoreHeart.src = scoreHeartList[score];
+        }else{
+          li.classList.add('wrong');
+        }      
+      }
+
+      btn.classList.remove('disabled');//「つぎ」押せるようにする
+    }
+
+    btn.addEventListener('click', ()=>{//「つぎ」ボタンを押したとき
+      
+      if(btn.classList.contains('disabled')){//未回答のときは無。
       return;
     }
     btn.classList.add('disabled');//未回答状態に戻す。「つぎ」押せなくする。
     
-    if(currentNum === quiz.length - 1){//最後の一問だった時
+    if(currentNum === quiz.length - 2){//三問終了済
       fin.classList.remove('hidden');//「柴犬は」が表示される。
-      // scoreLabel.textContent = `うまくいった回数：${score}/${quiz.length}`;//スコアの表示
+      
       
       if (score == 0) {//リンク先変更
         fin.href = '0nade_result.html';
